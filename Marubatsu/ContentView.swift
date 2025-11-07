9d049c3593c81953825d95580992630aececac23
Sayaka <sayaka.ssk30@gmail.com>
2025-11-08 08:35:43 +0900

    Initial Commit

---
+++ Marubatsu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marubatsu
+//
+//  Created by Sayaka Sasaki on 2025/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by Sayaka Sasaki on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
